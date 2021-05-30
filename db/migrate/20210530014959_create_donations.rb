db8d013f994c275b19f76c8caf0b4609d558f229
TECH99SUZUMURa <82789367+TECH99SUZUMURa@users.noreply.github.com>
2021-05-30 11:05:37 +0900

    マイグレーションファイルの記述を編集し、マイグレーションを実行

--- db/migrate/20210530014959_create_donations.rb
+++ db/migrate/20210530014959_create_donations.rb
@@ -1,6 +1,8 @@
 class CreateDonations < ActiveRecord::Migration[6.1]
   def change
     create_table :donations do |t|
+      t.integer    :price, null: false, index: true
+      t.references :user,  null: false, foreign_key: true
 
       t.timestamps
     end
