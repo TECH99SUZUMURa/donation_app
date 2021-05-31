56923260798596cafd788f279da6e178c26bca2b
TECH99SUZUMURa <82789367+TECH99SUZUMURa@users.noreply.github.com>
2021-05-31 17:16:02 +0900

    バリデーションの記述を修正

--- app/models/donation_address.rb
+++ app/models/donation_address.rb
@@ -3,7 +3,7 @@ class DonationAddress
   attr_accessor :price, :user_id, :postal_code, :prefecture, :city, :house_number, :building_name
 
   with_options presence: true do
-    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_eqal_to: 1000000, message: 'is valid'}
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 1000000, message: 'is valid'}
     validates :user_id
     validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
   end
